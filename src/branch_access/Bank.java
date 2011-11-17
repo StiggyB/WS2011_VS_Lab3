e5d7e6180887b49b51e5c2e8003f5f167f91966f
Benny <benjamin.burchard@gmail.com>
2011-11-17 17:45:35 +0100

    Bank and Konto first commit

---
+++ src/branch_access/Bank.java
@@ -0,0 +1,21 @@
+package branch_access;
+
+import cash_access.Account;
+
+public class Bank extends Manager {
+
+	private Account kto;
+	
+	@Override
+	public String createAccount(String owner) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean removeAccount(String accountID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
